2b970afdeef333198a97d2eece7ec11906146278
Muhammad Saimon <saimon.ctg@gmail.com>
2020-03-10 07:55:37 +0600

    convert raw string data to csv file using Commons-csv

--- src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,9 +1,12 @@
 package io.javabrains.coronavirustracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -20,6 +23,14 @@ public class CoronaVirusDataService {
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
-    }
+
+        StringReader csvBodyReader  = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record:records){
+            String state   = record.get("Province/State");
+            String country = record.get("Country/Region");
+            System.out.println(state);
+        }
+
+    }//end of fetch
 }
