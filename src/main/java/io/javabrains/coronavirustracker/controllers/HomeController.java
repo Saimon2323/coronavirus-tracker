950919d1ea4fbd7eaffb793249e08f1a07c71ed9
Saimon <saimon.ctg@gmail.com>
2021-02-04 17:41:09 +0600

    code modified - data source link added

--- src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
+++ src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
@@ -17,14 +17,22 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model){
+
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
-        int totalReportedCasesToday = allStats.stream().mapToInt(stat -> stat.getLocationTotalCasesToday()).sum();
-        int totalReportedCasesYesterday = allStats.stream().mapToInt(stat -> stat.getLocationTotalCasesYesterday()).sum();
-        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+
+//        int totalReportedCasesToday = allStats.stream().mapToInt(stat -> stat.getLocationTotalCasesToday()).sum();
+//        int totalReportedCasesYesterday = allStats.stream().mapToInt(stat -> stat.getLocationTotalCasesYesterday()).sum();
+//        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        // same
+        int totalReportedCasesToday = allStats.stream().mapToInt(LocationStats::getLocationTotalCasesToday).sum();
+        int totalReportedCasesYesterday = allStats.stream().mapToInt(LocationStats::getLocationTotalCasesYesterday).sum();
+        int totalNewCases = allStats.stream().mapToInt(LocationStats::getDiffFromPrevDay).sum();
+
         model.addAttribute("totalReportedCasesYesterday", totalReportedCasesYesterday);
         model.addAttribute("totalReportedCasesToday", totalReportedCasesToday);
         model.addAttribute("totalNewCases", totalNewCases);
         model.addAttribute("coronaRecords", allStats);
+
         return "home";
     }
 }
